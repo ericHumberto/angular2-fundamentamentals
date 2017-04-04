af0c947a56a8eb7dc6cf3522e8d7bba37e1f6bf2
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-04 10:17:48 -0300

    Click button parente to child

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'
+import { Component, Input, Output, EventEmitter } from '@angular/core'
 
 @Component({
     selector: 'event-thumbnail',
@@ -14,15 +14,16 @@ import { Component, Input } from '@angular/core'
             <span>{{event.location.city}}, {{event.location.country}}</span>
         </div>
         <button class="btn btn-primary" (click)="handleClickMe()">
-            Click handleClickMe
+            Click handle
         </button>
     </div>
     `
 })
 export class EventThumbnailComponent {
     @Input() event: any
+    @Output() eventClick = new EventEmitter()
 
     handleClickMe() {
-        console.log('Clicked!');
+        this.eventClick.emit('foo');
     }
 }
