9f584da56aadeaabc42724a8efb9c98bb1ad37d7
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-05 13:57:21 -0300

    ngClass

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -6,7 +6,7 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
     <div class="well hoverwell thumbnail">
         <h2>{{event.name}}</h2>
         <div>Date: {{event.date}}</div>
-        <div [ngSwitch]="event?.time">
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
             Time: {{event.time}}
             <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
             <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -17,12 +17,14 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
             Online Url: {{event.onlineUrl}}
         </div>
         <div [hidden]="!event?.location">
-            <span>Location: {{event.location.address}}</span>
-            <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+            <span>Location: {{event?.location.address}}</span>
+            <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
         </div>        
     </div>
     `,
     styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height:210px }
     .pad-left { margin-left : 10px }
     .well div { color: #bbb }
@@ -30,4 +32,12 @@ import { Component, Input, Output, EventEmitter } from '@angular/core'
 })
 export class EventThumbnailComponent {
     @Input() event: any
+
+    getStartTimeClass() {
+        if (this.event && this.event.time === '8:00 am') {
+            return ['green', 'bold'];
+        }
+
+        return [];
+    }
 }
