d03fb92a233b8065623c75824b120dd10560ffc8
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-06 14:17:30 -0300

    activate (url handle)

--- app/app.module.ts
+++ app/app.module.ts
@@ -6,25 +6,28 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavBarComponent } from './nav/navbar.component'
-import { EventService} from './events/shared/event.service'
+import { EventService } from './events/shared/event.service'
 import { ToastrService } from './common/toastr.service'
 import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
 import { CreateEventComponent } from './events/create-event.component'
+import { Error404Component } from './errors/404.component'
+import { EventRouteActivator } from './events/event-details/event-route-activator.service'
 
 @NgModule({
     imports: [BrowserModule,
         RouterModule.forRoot(appRoutes)
-        ],
+    ],
     declarations: [
         EventsAppComponent,
         EventsListComponent,
         EventThumbnailComponent,
         NavBarComponent,
         EventDetailsComponent,
-        CreateEventComponent
+        CreateEventComponent,
+        Error404Component
     ],
-    providers : [EventService, ToastrService],
+    providers: [EventService, ToastrService, EventRouteActivator],
     bootstrap: [EventsAppComponent]
 })
 
