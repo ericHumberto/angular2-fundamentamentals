e25bf6d4a68ba0152ec0de94349ef0dbb4bcfbcb
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-06 13:30:49 -0300

    Route navigation from code

---
+++ app/events/create-event.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core'
+import { Router } from '@angular/router'
+
+@Component({
+    template: `
+    <h1>New event</h1>
+    <hr>
+    <div class="col-md-6">
+        <h3>[Create event form will go here]</h3>
+        <br/>
+        <br/>
+        <button type="submit" class="btn btn-primary">Save</button>
+        <button type="submit" class="btn btn-default" (click)="clickCancel()">Cancel</button>
+    </div>
+    `
+})
+export class CreateEventComponent {
+    constructor(private router: Router) {
+    }
+
+    clickCancel() {
+        this.router.navigate(['/events']);
+    }
+
+}
