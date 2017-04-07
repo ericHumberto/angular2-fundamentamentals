fe3c08db579e4f8a3e9e1cf2054a5ad5a5e113ad
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-07 11:08:38 -0300

    Form submit

--- app/user/user.module.ts
+++ app/user/user.module.ts
@@ -1,16 +1,21 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { RouterModule } from '@angular/router'
+
 import { userRoutes } from './user.routes'
 import { ProfileComponent } from './profile.component'
+import { LoginComponent } from './login.component';
 
 @NgModule({
     imports: [
         CommonModule,
+        FormsModule,
         RouterModule.forChild(userRoutes)
     ],
     declarations:[
-        ProfileComponent
+        ProfileComponent,
+        LoginComponent
     ],
     providers:[]
 })
