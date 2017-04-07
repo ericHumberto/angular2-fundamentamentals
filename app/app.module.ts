9795e2c53a3811e466fe160116a239409eb7ffbd
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-07 10:23:09 -0300

    Organizing exports

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,21 @@ import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { RouterModule } from '@angular/router'
 
-import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
-import { NavBarComponent } from './nav/navbar.component'
-import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
 import { appRoutes } from './routes'
-import { CreateEventComponent } from './events/create-event.component'
+import { ToastrService } from './common/toastr.service'
+import { NavBarComponent } from './nav/navbar.component'
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivator } from './events/event-details/event-route-activator.service'
-import { EventListResolver } from './events/event-list-resolver.service'
+import { EventsAppComponent } from './events-app.component'
+
+import {
+    EventsListComponent,
+    EventThumbnailComponent,
+    EventService,
+    EventDetailsComponent,
+    CreateEventComponent,
+    EventRouteActivator,
+    EventListResolver
+} from './events/index'
 
 @NgModule({
     imports: [BrowserModule,
@@ -43,8 +46,8 @@ import { EventListResolver } from './events/event-list-resolver.service'
 
 export class AppModule { }
 
-function checkDirtyState(component:CreateEventComponent) : boolean {
-  if (component.isDirty)
-    return window.confirm('You have not saved this event, do you really want to cancel?')
-  return true
+function checkDirtyState(component: CreateEventComponent): boolean {
+    if (component.isDirty)
+        return window.confirm('You have not saved this event, do you really want to cancel?')
+    return true
 }
