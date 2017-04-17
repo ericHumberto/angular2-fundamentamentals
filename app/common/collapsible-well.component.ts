3cd3243f6b65f4585ea36626e732cf45813ad950
Eric Humberto Cypriano de Souza <erics@cit>
2017-04-17 16:02:06 -0300

    Multiple content ng-content

--- app/common/collapsible-well.component.ts
+++ app/common/collapsible-well.component.ts
@@ -4,14 +4,13 @@ import { Component, OnInit, Input } from '@angular/core';
     selector: 'collapsible-well',
     template: `
     <div (click)="toggleContent()" class="well pointable">
-        <h4 class="well-title">{{title}}</h4>
-        <ng-content *ngIf="visible"></ng-content>
+        <ng-content select="[well-title]"></ng-content>
+        <ng-content select="[well-body]" *ngIf="visible"></ng-content>
     </div>
     `
 })
 
 export class CollapsibleWellComponent implements OnInit {
-    @Input() title: string;
     visible: boolean = true;
 
     constructor() { }
